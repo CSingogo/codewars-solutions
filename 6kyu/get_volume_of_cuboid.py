ddf2a8f40d1da8b7bc435252da340f493ec40d44
CSingogo <singogosingogo@gmail.com>
2024-01-13 18:56:58 +0200

    solutions added

---
+++ 6kyu/get_volume_of_cuboid.py
@@ -0,0 +1,8 @@
+"""
+
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+"""
+
+#solution
+get_volume_of_cuboid = lambda length, width, height: length * width * height
---
+++ 6kyu/reverse_string.py
@@ -0,0 +1,15 @@
+"""
+
+This question is asked by Google. Given a string, reverse all of its characters and return the resulting string.
+
+Ex: Given the following strings...
+
+“Cat”, return “taC”
+“The Daily Byte”, return "etyB yliaD ehT”
+“civic”, return “civic”
+
+"""
+
+#solution
+
+reverse = lambda str: ''.join(reversed(str))
