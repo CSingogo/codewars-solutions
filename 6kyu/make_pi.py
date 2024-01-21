ef69ce6aa90849ecedfcf63956480e621e136bdd
CSingogo <singogosingogo@gmail.com>
2024-01-21 23:33:02 +0200

    solutions added

---
+++ 6kyu/make_pi.py
@@ -0,0 +1,13 @@
+'''
+
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+make_pi() → [3, 1, 4]
+
+'''
+
+## solution
+
+make_pi = lambda : [3,1,4]
---
+++ 6kyu/string_times.py
@@ -0,0 +1,15 @@
+"""
+
+
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+
+"""
+
+##solution
+
+string_times = lambda str, n : str * n
